2cc91b8405ee3031a6c9c8cee773e3438e3cdf3b
Vijay <vijayrauniyar1818@gmail.com>
2021-03-27 19:53:14 +0530

    task1 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router,Route,Redirect} from 'react-router-dom';
 import './App.css';
+import Users from './user/pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact>
+      <Users/>
+      </Route>
+      <Redirect to="/"/>
+    </Router>
   );
 }
 
---
+++ src/user/pages/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Users=()=>{
+return <h1> Hello</h1>    
+
+}
+
+export default Users;
