948722e23f43283fbeae45e92e6cd7c31d2510b3
Vijay <vijayrauniyar1818@gmail.com>
2021-03-28 17:23:02 +0530

    chnges

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Redirect} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
 import './App.css';
 import Users from './user/pages/Users';
+import NewPlaces from "./places/pages/NewPlaces";
 
 function App() {
   return (
     <Router>
+      <Switch>
       <Route path="/" exact>
       <Users/>
       </Route>
+      <Route path="/places/new"><NewPlaces/></Route>
       <Redirect to="/"/>
+      </Switch>
     </Router>
+    
   );
 }
 
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const NewPlaces=()=>{
+return <h1> Hello New Places</h1>    
+
+}
+
+export default NewPlaces;
